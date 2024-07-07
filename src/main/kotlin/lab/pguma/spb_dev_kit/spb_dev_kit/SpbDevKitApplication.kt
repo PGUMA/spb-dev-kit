fd6948c35dd8bea7caff6cdfc07782e52168db59
pguma <p.guma.service+git@gmail.com>
2024-07-07 23:16:03 +0900

    feat: spring initialize

---
+++ src/main/kotlin/lab/pguma/spb_dev_kit/spb_dev_kit/SpbDevKitApplication.kt
@@ -0,0 +1,11 @@
+package lab.pguma.spb_dev_kit.spb_dev_kit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpbDevKitApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpbDevKitApplication>(*args)
+}
---
+++ src/test/kotlin/lab/pguma/spb_dev_kit/spb_dev_kit/SpbDevKitApplicationTests.kt
@@ -0,0 +1,13 @@
+package lab.pguma.spb_dev_kit.spb_dev_kit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpbDevKitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
