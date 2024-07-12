d15ea51c3725fb2a1fd8cb6a0b2607992a90a89b
pguma <p.guma.service+git@gmail.com>
2024-07-12 18:01:16 +0900

    feat: introduce version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,9 @@ buildscript {
 plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.kotlin.jvm)
 
     id("org.flywaydb.flyway") version "10.15.2"
 
