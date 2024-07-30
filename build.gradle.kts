ac6d89b9223f552f5b74aac0b916422287bdcbd9
pguma <p.guma.service+git@gmail.com>
2024-07-30 23:28:50 +0900

    refact: structure order change

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,19 +44,19 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     runtimeOnly("org.flywaydb:flyway-database-postgresql:10.15.0")
 
-    jooqGenerator("org.postgresql:postgresql")
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.resend:resend-java:3.1.0")
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("testImplementation 'io.rest-assured:rest-assured:5.5.0'")
 
+    jooqGenerator("org.postgresql:postgresql")
     spotbugsPlugins("com.h3xstream.findsecbugs:findsecbugs-plugin:1.7.1")
 }
 
